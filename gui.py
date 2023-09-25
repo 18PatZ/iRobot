ca70bd2e17f2bddbbf41d7de983024d76e393d1f
18PatZ <18patz@students.harker.org>
2023-09-25 17:53:30 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,20 +1,36 @@
 from tkinter import *
 from tkinter import ttk
 import os
+import re
 
-from irobot_interface import iRobotInterface
+from irobot_interface import iRobotInterface, NOTES
 
 w = None
 
 pressed = set()
 robot = None
 
+KEY_NOTE_MAP = {
+    "q": "C",
+    "w": "D",
+    "e": "E",
+    "r": "F",
+    "t": "G",
+    "y": "A",
+    "u": "B",
+    "i": "C_1",
+    "o": "D_1",
+    "p": "E_1",
+    "bracketleft": "F_1",
+    "bracketright": "G_1"
+}
+
 def key_pressed(event):
     if event.keysym == 'Escape':
         exit()
     print("PRESSED",event.keysym)
     # w.config(text="Key Pressed:"+event.char)
-    pressed.add(event.keysym)
+    pressed.add(event.keysym.lower())
     # print()
     update()
 
@@ -22,7 +38,7 @@ def key_pressed(event):
 def key_released(event):
     # w.config(text = "Key Released:"+event.char)
     print("RELEASED",event.keysym)
-    pressed.remove(event.keysym)
+    pressed.remove(event.keysym.lower())
     update()
 
 
@@ -30,13 +46,13 @@ def init_robot():
     global robot
 
     robot = iRobotInterface(use_serial = False, use_TCP = True, 
-        tcp_ip = '127.0.0.1', 
+        tcp_ip = '10.228.106.75',#'127.0.0.1', 
         tcp_port = 6666)
 
     robot.start_mode()
     robot.safe_mode()
     robot.register_beeps()
-    robot.beep(0)
+    # robot.beep(3)
 
 
 def init():
@@ -67,10 +83,43 @@ def update():
         t += ("" if len(t) == 0 else ", ") + c
     w.config(text="Keys pressed: " + t)
 
-    if "w" in pressed:
-        robot.drive(speed = 200)
+    sp = 500
+    # if "shift_l" in pressed:
+    #     sp = 500
+
+    ra = 250
+    if "shift_l" in pressed:
+        ra = 100
+
+    duration = 12
+    if "shift_l" in pressed:
+        duration = 36
+
+    turn_radius = ra if "right" in pressed else (-ra if "left" in pressed else None)
+
+    if "up" in pressed:
+        robot.drive(speed = sp, turn_radius = turn_radius)
+    elif "down" in pressed:
+        robot.drive(speed = -sp, turn_radius = turn_radius)
+    elif "right" in pressed:
+        robot.turn(speed = sp)
+    elif "left" in pressed:
+        robot.turn(speed = -sp)
     else:
         robot.stop()
+
+    for p in pressed:
+        if re.search("^[0-9]$", p) is not None:
+            robot.beep(int(p))
+            break
+
+    # if "space" in pressed:
+    #     robot.beep(8)
+
+    for note in KEY_NOTE_MAP:
+        if note in pressed:
+            robot.register_beep(10, [(NOTES[KEY_NOTE_MAP[note]], duration)])
+            robot.beep(10)
 
 # disable X's autorepeat behaviour - holding down key causes continuous presses and releases
 # os.system('xset r off')
